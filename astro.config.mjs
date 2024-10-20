31cd9c40f06ecf543681080649a73b3264a09bbb
Rudi Farkas <rudi.farkas@gmail.com>
2024-10-20 16:17:30 +0200

    add @astro/lit d3; fix eslint 8.56 .eslintrc.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 import vercel from '@astrojs/vercel/static';
+import lit from '@astrojs/lit';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
-  integrations: [],
+  integrations: [lit()],
   vite: {
     plugins: [
       viteStaticCopy({
